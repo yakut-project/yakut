000824321e0712fe69d0d4bde100343c5290d624
Muhammet DİLEK <dilekmuhammet@gmail.com>
2013-09-05 12:14:49 +0300

    #16 view helper moved to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def page(title, id)
+    @page_id = id
+    content_for :title do
+      title
+    end
+  end
+
+  def launcher_active(page)
+    @page_id == page ? {class:'active'} : {}
+  end
 end
