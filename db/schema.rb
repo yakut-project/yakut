6a88594a2ceaf0c87d68962f5f3c9afb289243f3
Muhammet DİLEK <dilekmuhammet@gmail.com>
2013-09-11 14:47:48 +0300

    scheme.rb fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911070035) do
+ActiveRecord::Schema.define(version: 20130905072700) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(version: 20130911070035) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.string   "barcode"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
